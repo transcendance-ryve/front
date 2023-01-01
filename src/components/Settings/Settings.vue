12c37b9f08be2092b6eddd1e59778c3ee8898ca1
vintran <vintran@student.42.fr>
2023-01-01 20:59:02 +0100

    Settings done

--- src/components/Settings/Settings.vue
+++ src/components/Settings/Settings.vue
@@ -51,6 +51,7 @@
 	<div class="mainContent-settings">
 		<div class="Settings-wrapper">
 			<div class="Setting Setting--profile">
+				<div class="Setting-tag"><span class="Tag-value">Profile</span></div>
 				<UploadAvatar :avatar="avatar" @change="uploadAvatar"/>
 				<BaseInput
 					v-model="username"
@@ -60,6 +61,7 @@
 				/>
 			</div>
 			<div class="Setting Setting-password">
+				<div class="Setting-tag"><span class="Tag-value">Password</span></div>
 				<h2 class="Setting-label">Change password</h2>
 				<div class="Passwords-wrapper">
 					<BaseInput
@@ -83,6 +85,7 @@
 				</div>
 			</div>
 			<div class="Setting Setting-password">
+				<div class="Setting-tag"><span class="Tag-value">2FA</span></div>
 				<div class="Label-wrapper">
 					<h2 class="Setting-label">Actived / Disabled 2FA</h2>
 					<ToggleSwitch :active="active2FA" @click="active2FA = !active2FA"/>
