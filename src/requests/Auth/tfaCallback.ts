14c659efcadc7aae093b31497a422b133e930f86
vintran <vintran@student.42.fr>
2023-01-07 09:16:29 +0100

    2FA :heavy_check_mark:

---
+++ src/requests/Auth/tfaCallback.ts
@@ -0,0 +1,25 @@
+import useAxios from '@/requests/useAxios'
+import router from '@/router'
+import { useUserStore } from '@/stores/UserStore'
+
+const tfaCallback = async (code: string) => {
+	const	id = router.currentRoute.value.query.id
+	const	token = router.currentRoute.value.query.token
+
+	const { response, loading, error } = await useAxios(
+		'post',
+		'/auth/tfa/callback',
+		{ id, token, code }
+	)
+	if (error.value) {
+		//	handle errors
+	}
+	else if (response.value) {
+		const	userStore = useUserStore()
+		userStore.updateMe(response.value)
+		userStore.loginApi = true
+		router.push({ path: '/' })
+	}
+}
+
+export default tfaCallback
