250d32b25f30c317e02ddccfb929babb416cc2f5
vintran <vintran@student.42.fr>
2023-01-09 11:27:24 +0100

    clean

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
 import './styles/index.scss'
 
-const app = createApp(App)
+const	app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
