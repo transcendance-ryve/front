85412460a836a6847933d3bb0fd6f21395e11f14
vintran <vintran@student.42.fr>
2023-01-09 13:57:39 +0100

    clean

--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -8,7 +8,7 @@ export const useUserStore = defineStore('userStore', () => {
 
 	const	loginApi: Ref = ref(localStorage.getItem('me') ? true : false)
 
-	const me: Ref = ref(loginApi.value ? JSON.parse(localStorage.getItem('me') || '') : '')
+	const	me: Ref = ref(loginApi.value ? JSON.parse(localStorage.getItem('me') || '') : '')
 
 	function	updateMe(data: any) {
 		localStorage.setItem('me', JSON.stringify(data))
