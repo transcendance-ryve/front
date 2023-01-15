2af7a529271a50c5a17d21b0c510a614c88b4431
vintran <vintran@student.42.fr>
2023-01-15 19:23:18 +0100

    socket event fix in progress

--- src/components/Notifications/Notifications.vue
+++ src/components/Notifications/Notifications.vue
@@ -4,53 +4,54 @@
 	import NotifTag from './NotifTag.vue'
 	import { useUserStore } from '@/stores/UserStore'
 	import { useNotifStore } from '@/stores/NotificationsStore'
+	import type { SocketEvent } from '@/types/Socket';
 
 	const	notifStore = useNotifStore()
 	const	userStore = useUserStore()
 	const	socket: any = userStore.socket
 
-	const	listeners: any[] = []
+	const	listeners: SocketEvent[] = [
+		{ name: 'user_connected', callback: (res: any) => notifStore.addNotif('infoG', '', 'connected') },
+		{ name: 'friend_accepted', callback: (res: any) => notifStore.addNotif('infoG', res.username, 'new friend', res.avatar) },
+		{ name: 'friend_accepted_submitted', callback: (res: any) => notifStore.addNotif('infoG', res.username, 'new friend', res.avatar) },
+		{ name: 'friend_request_submitted', callback: (res: any) => notifStore.addNotif('infoG', res.username, 'invitation sent', res.avatar) },
+		{ name: 'invitationAccepted', callback: () => notifStore.addNotif('infoG', '', 'invitation accepted') },
+		{ name: 'invitationSent', callback: (res: any) => notifStore.addNotif('infoG', res.username, 'invitation sent', res.avatar) },
+
+		{ name: 'user_disconnected', callback: (id: string) => notifStore.addNotif('infoR', '', 'disconnected') },
+		{ name: 'friend_declined', callback: (res: any) => notifStore.addNotif('infoR', res.username, 'declined your invitation', res.avatar) },
+		{ name: 'friend_removed', callback: (res: any) => notifStore.addNotif('infoR', res.username, 'has deleted you', res.avatar) },
+		{ name: 'friend_declined_submitted', callback: (res: any) => notifStore.addNotif('infoR', res.username, 'invitation declined', res.avatar) },
+		{ name: 'friend_removed_submitted', callback: (res: any) => notifStore.addNotif('infoR', res.username, 'friend removed', res.avatar) },
+		{ name: 'invitationDeclined', callback: () => notifStore.addNotif('infoR', '', 'invitation declined') },
+		{ name: 'roomLeft', callback: () => notifStore.addNotif('infoR', '', 'channel leaved') },
+
+		{ name: 'friend_request', callback: (res: any) => notifStore.addNotif('infoB', res.username, 'new friend invitation', res.avatar) },
+		{ name: 'chanInvitationReceived', callback: (res: any) => notifStore.addNotif('infoY', res.name, 'new channel invitation', res.avatar) },
+
+		{ name: 'roomCreated', callback: () => notifStore.addNotif('success', 'Success', 'Channel created') },
+		{ name: 'joinRoomSuccess', callback: () => notifStore.addNotif('success', 'Success', 'channel joined') },
+
+		{ name: 'acceptInvitationFailed', callback: (err: string) => notifStore.addNotif('error', 'Error', err) },
+		{ name: 'messageRoomFailed', callback: (err: string) => notifStore.addNotif('error', 'Error', err) },
+		{ name: 'inviteToRoomFailed', callback: (err: string) => notifStore.addNotif('error', 'Error', err) },
+		{ name: 'createRoomFailed', callback: (err: string) => notifStore.addNotif('error', 'Error', err) },
+		{ name: 'joinRoomFailed', callback: () => notifStore.addNotif('error', 'Error', 'Cannot join channel') },
+		{ name: 'promoteUserFailed', callback: (err: string) => notifStore.addNotif('error', 'Error', err) },
+		{ name: 'demoteUserFailed', callback: (err: string) => notifStore.addNotif('error', 'Error', err) },
+		{ name: 'muteUserFailed', callback: (err: string) => notifStore.addNotif('error', 'Error', err) },
+		{ name: 'unmuteUserFailed', callback: (err: string) => notifStore.addNotif('error', 'Error', err) },
+		{ name: 'banUserFailed', callback: (err: string) => notifStore.addNotif('error', 'Error', err) },
+		{ name: 'unbanUserFailed', callback: (err: string) => notifStore.addNotif('error', 'Error', err) },
+	]
+
 
 	onMounted(() => {
-		// listeners.push(socket.on('pong', () => { notifStore.addNotif('', 'Pong', '', '') }))
-		// socket.emit('ping', () => { console.log('ping emit') })
-
-		listeners.push(socket.on('user_connected', (res: any) => {notifStore.addNotif('infoG', '', 'connected'); console.log('connected', res)}))
-		listeners.push(socket.on('friend_accepted', (res: any) => { notifStore.addNotif('infoG', res.username, 'new friend', res.avatar) }))
-		listeners.push(socket.on('friend_accepted_submitted', (res: any) => notifStore.addNotif('infoG', res.username, 'new friend', res.avatar)))
-		listeners.push(socket.on('friend_request_submitted', (res: any) => notifStore.addNotif('infoG', res.username, 'invitation sent', res.avatar)))
-		listeners.push(socket.on('invitationAccepted', () => notifStore.addNotif('infoG', '', 'invitation accepted')))
-		listeners.push(socket.on('invitationSent', (res: any) => notifStore.addNotif('infoG', res.username, 'invitation sent', res.avatar)))
-
-		listeners.push(socket.on('user_disconnected',  (id: string) => notifStore.addNotif('infoR', '', 'disconnected')))
-		listeners.push(socket.on('friend_declined', (res: any) => notifStore.addNotif('infoR', res.username, 'declined your invitation', res.avatar)))
-		listeners.push(socket.on('friend_removed', (res: any) => notifStore.addNotif('infoR', res.username, 'has deleted you', res.avatar)))
-		listeners.push(socket.on('friend_declined_submitted', (res: any) => notifStore.addNotif('infoR', res.username, 'invitation declined', res.avatar)))
-		listeners.push(socket.on('friend_removed_submitted', (res: any) => notifStore.addNotif('infoR', res.username, 'friend removed', res.avatar)))
-		listeners.push(socket.on('invitationDeclined', () => notifStore.addNotif('infoR', '', 'invitation declined')))
-		listeners.push(socket.on('roomLeft', () => notifStore.addNotif('infoR', '', 'channel leaved')))
-
-		listeners.push(socket.on('friend_request', (res: any) => notifStore.addNotif('infoB', res.username, 'new friend invitation', res.avatar)))
-		listeners.push(socket.on('chanInvitationReceived', (res: any) => notifStore.addNotif('infoY', res.name, 'new channel invitation', res.avatar)))
-
-		listeners.push(socket.on('roomCreated', () => notifStore.addNotif('success', 'Success', 'Channel created')))
-		listeners.push(socket.on('joinRoomSuccess', () => notifStore.addNotif('success', 'Success', 'channel joined')))
-		
-		listeners.push(socket.on('acceptInvitationFailed', (err: string) => notifStore.addNotif('error', 'Error', err)))
-		listeners.push(socket.on('inviteToRoomFailed', (err: string) => notifStore.addNotif('error', 'Error', err)))
-		listeners.push(socket.on('createRoomFailed', (err: string) => notifStore.addNotif('error', 'Error', err)))
-		listeners.push(socket.on('joinRoomFailed', () => notifStore.addNotif('error', 'Error', 'Cannot join channel')))
-		listeners.push(socket.on('promoteUserFailed', (err: string) => notifStore.addNotif('error', 'Error', err)))
-		listeners.push(socket.on('demoteUserFailed', (err: string) => notifStore.addNotif('error', 'Error', err)))
-		listeners.push(socket.on('muteUserFailed', (err: string) => notifStore.addNotif('error', 'Error', err)))
-		listeners.push(socket.on('messageRoomFailed', (err: string) => notifStore.addNotif('error', 'Error', err)))
-		listeners.push(socket.on('unmuteUserFailed', (err: string) => notifStore.addNotif('error', 'Error', err)))
-		listeners.push(socket.on('banUserFailed', (err: string) => notifStore.addNotif('error', 'Error', err)))
-		listeners.push(socket.on('unbanUserFailed', (err: string) => notifStore.addNotif('error', 'Error', err)))
+		listeners.forEach(listener => socket.on(listener.name, listener.callback))
 	})
 
 	onUnmounted(() => {
-		listeners.forEach(listener => socket.off(listener))
+		listeners.forEach(listener => socket.off(listener.name, listener.callback))
 	})
 
 </script>
@@ -63,33 +64,7 @@
 			:key="notif.id"
 			:notif="notif"
 		/>
-		<!-- <NotifTag
-			:notif="{
-				type: 'friend',
-				id: '1',
-				label: 'vintran',
-				text: 'new friend invitation',
-				avatar: 'http://localhost:3000/default.png'
-			}"
-		/>
-		<NotifTag
-			:notif="{
-				type: 'success',
-				id: '2',
-				label: 'fwihk',
-				text: 'oadij edow ifb riwfbd d qndo woeifn',
-				avatar: 'http://localhost:3000/default.png'
-			}"
-		/>
-		<NotifTag
-			:notif="{
-				type: 'channel',
-				id: '3',
-				label: 'RYVE',
-				text: 'new channel invitation',
-				avatar: 'http://localhost:3000/default.png'
-			}"
-		/> -->
 	</div>
 
 </template>
+
