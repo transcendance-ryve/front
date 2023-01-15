82669e5f0bc413b228346038865c34e3933596aa
Sethhhhhhh <yohannvvt@gmail.com>
2023-01-15 18:10:33 +0100

    force

--- src/components/Play/Game.vue
+++ src/components/Play/Game.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 	import VersusTag from '@/components/Spectate/VersusTag.vue'
 	import { useUserStore } from '@/stores/UserStore'
 	import { ref, type Ref, onMounted, onUnmounted } from 'vue';
@@ -17,11 +16,6 @@
 	interface Players {
 		left: Player,
 		right: Player,
-	}
-
-	interface Grid {
-		width: number,
-		height: number,
 	}
 
 	interface Keys {
@@ -63,9 +57,14 @@
 	const userStore = useUserStore()
 	const socket = userStore.socket;
 	const canvas = ref(null);
-	const listeners: any = [];
 
-	let players: Ref<Players> = ref({
+	interface Props {
+		spectate?: boolean,
+	}
+
+	const props: Props = defineProps<Props>();
+
+	const players: Ref<Players> = ref({
 		left: {
 			id: '',
 			username: 'Player 1',
@@ -86,10 +85,7 @@
 		},
 	})
 
-
-
-
-	let defaultGrid: DefaultGrid = {
+	const defaultGrid: DefaultGrid = {
 		width: 0,
 		height: 0,
 	};
@@ -100,60 +96,53 @@
 		up: false,
 		down: false,
 	}
+	
 
-	/* onMounted && onUnmounted */
+	const handleKeyup = (e: any) => {
+		switch (e.key) {
+			case 'w':
+				keys.w = false;
+				socket.emit('keyrelease', 'up');
+				break;
+			case 's':
+				keys.s = false;
+				socket.emit('keyrelease', 'down');
+				break;
+			case 'ArrowUp':
+				keys.up = false;
+				socket.emit('keyrelease', 'up');
+				break;
+			case 'ArrowDown':
+				keys.down = false;
+				socket.emit('keyrelease', 'down');
+				break;
+		}
+	}
 
-	onMounted(() => {
-		window.addEventListener('keyup', (e) => {
-			switch (e.key) {
-				case 'w':
-					keys.w = false;
-					socket.emit('keyrelease', 'up');
-					break;
-				case 's':
-					keys.s = false;
-					socket.emit('keyrelease', 'down');
-					break;
-				case 'ArrowUp':
-					keys.up = false;
-					socket.emit('keyrelease', 'up');
-					break;
-				case 'ArrowDown':
-					keys.down = false;
-					socket.emit('keyrelease', 'down');
-					break;
-			}
-		})
-
-		window.addEventListener('keydown', (e) => {
-			switch (e.key) {
-				case 'w':
-					if (keys.w) break;
-					keys.w = true;
-					socket.emit('keypress', 'up');
-					break;
-				case 's':
-					if (keys.s) break;
-					keys.s = true;
-					socket.emit('keypress', 'down');
-					break;
-				case 'ArrowUp':
-					if (keys.up) break;
-					keys.up = true;
-					socket.emit('keypress', 'up');
-					break;
-				case 'ArrowDown':
-					if (keys.down) break;
-					keys.down = true;
-					socket.emit('keypress', 'down');
-					break;
-			}
-		})
-	})
-
-	onUnmounted(() => {
-		listeners.forEach((listener: any) => socket.off(listener));
-	})
+	const handleKeydown = (e: any) => {
+		switch (e.key) {
+			case 'w':
+				if (keys.w) break;
+				keys.w = true;
+				socket.emit('keypress', 'up');
+				break;
+			case 's':
+				if (keys.s) break;
+				keys.s = true;
+				socket.emit('keypress', 'down');
+				break;
+			case 'ArrowUp':
+				if (keys.up) break;
+				keys.up = true;
+				socket.emit('keypress', 'up');
+				break;
+			case 'ArrowDown':
+				if (keys.down) break;
+				keys.down = true;
+				socket.emit('keypress', 'down');
+				break;
+		}
+	}
 
 	/* Draw */
 
@@ -227,7 +216,7 @@
 	/* Socket handler */
 
 	const update = (game: { paddles: Paddles, ball: Ball }): void => {
-		// console.log(game);
+		if (!canvas.value) return;
 		const ctx = canvas.value.getContext('2d');
 		
 		const ratio = {
@@ -256,24 +245,36 @@
 			players.value.right.score = data.score;
 		}
 	}
-	// const playerJoin = (): void => {}
-	// const bonusSpawn = (): void => {}
-	// const bonusTaken = (): void => {}
 
-	/* Socket listeners */
+	const listeners: any = {
+		start: start,
+		update: update,
+		score: updateScore
+	}
+
+	/* onMounted && onUnmounted */
 
 	onMounted(() => {
-		listeners.push(socket.on("start", (data: { players: Players, width: number, height: number }) => { start(data) }));
-		listeners.push(socket.on("update", (game: { paddles: Paddles, ball: Ball }) => { update(game) }));
-		listeners.push(socket.on("score", (data: { id: string, score: number }) => { updateScore(data) }));
-		// listeners.push(socket.on("bonus_spawn", (data: any) => bonusSpawn() ))
-		// listeners.push(socket.on("bonus_taken", (data: any) => bonusTaken() ))
+		for (let name in listeners) {
+			socket.on(name, listeners[name]);
+		}
+
+		if (!props.spectate) {
+			window.addEventListener('keyup', handleKeyup, true);
+			window.addEventListener('keydown', handleKeydown, true);
+		}
 	})
 
 	onUnmounted(() => {
-		listeners.forEach((listener: any) => socket.off(listener));
-	})
+		for (let name in listeners) {
+			socket.off(name, listeners[name]);
+		}
 
+		if (!props.spectate) {
+			window.removeEventListener('keydown', handleKeydown, true);
+			window.removeEventListener('keyup', handleKeyup, true);
+		}
+	})
 </script>
 
 <template>
