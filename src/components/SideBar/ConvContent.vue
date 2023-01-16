9659c8a71568a85f65c428c30897523b9ae475af
Sethhhhhhh <yohannvvt@gmail.com>
2023-01-16 02:07:03 +0100

    scroll to last message.

--- src/components/SideBar/ConvContent.vue
+++ src/components/SideBar/ConvContent.vue
@@ -1,16 +1,28 @@
 <script setup lang="ts">
 
+	import { ref, onMounted, watch } from 'vue';
 	import Message from './Message.vue';
 
 	export interface Props {
 		messages: any[]
 	}
 
-	defineProps<Props>()
+	const props = defineProps<Props>()
+
+	// scroll to bottom when new message
+	const messagesRefs = ref(null);
+
+	watch(messagesRefs, (newMessagesRefs: any) => {
+		if (newMessagesRefs) {
+			const el = newMessagesRefs[props.messages.length - 1].$el;
+			if (el) el.scrollIntoView();
+		}
+	});
+
 </script>
 
 <template>
-	<div class="message__list">
+	<div class="message__list" ref="messagesContainer">
 		<Message
 			v-for="(msg, index) in messages"
 			:key="index"
@@ -18,6 +30,7 @@
 			:sameUser="messages[index + 1]?.sender.id === msg.sender.id"
 			:newUser="messages[index - 1]?.sender.id !== msg.sender.id || index === 0 || !messages[index - 1]"
 			:lastUser="index === messages.length - 1"
+			ref="messagesRefs"
 		/>
 	</div>
 </template>
