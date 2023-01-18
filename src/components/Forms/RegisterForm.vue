09dbee0b275e0d289d207f2ebd5a144fe5f08071
vintran <vintran@student.42.fr>
2023-01-18 18:16:30 +0100

    clean

--- src/components/Forms/RegisterForm.vue
+++ src/components/Forms/RegisterForm.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-	import { ref, reactive, computed, onMounted } from 'vue'
+	import { reactive, computed, onMounted } from 'vue'
 	import BaseInput from '../Utils/BaseInput.vue'
 	import useVuelidate from '@vuelidate/core'
 	import {
